03d9dd058292d2ae57692d10512cfb7ca517247d
jesfro1124 <jessika.frojmark@gmail.com>
2025-06-11 14:34:21 +0200

    bytt så den kollar på objektets pos istället för i arrayen, la till funktionalitet för at inte spawna i tre i rad igen. Försökt fixa debug för om det är tre i rad eller ej.

--- Candy Crush/Assets/Scripts/Grid.cs
+++ Candy Crush/Assets/Scripts/Grid.cs
@@ -21,8 +21,18 @@ public class Grid : MonoBehaviour
         {
             for (int y = 0; y < size; y++)
             {
-                //spawn grid
-                int index = Random.Range(0, boxOptions.Length);
+                int index;
+
+                do
+                {
+                    index = Random.Range(0, boxOptions.Length);
+                    // no three in row
+                } while (x >= 2 &&
+                    gridItems[x - 1, y] != null &&
+                    gridItems[x - 2, y] != null &&
+                    gridItems[x - 1, y].boxIndex == index &&
+                    gridItems[x - 2, y].boxIndex == index);
+
                 Vector3 pos = new Vector3(x * spacing, y * spacing, 0);
                 Instantiate(box, pos, Quaternion.identity, transform);
                 GameObject go = Instantiate(boxOptions[index], pos, Quaternion.identity, transform);
@@ -38,27 +48,63 @@ public class Grid : MonoBehaviour
         }
     }
 
+
     public void SwapItems(GridItem a, GridItem b)
     {
+        // old kordinates
+        int ax = a.x;
+        int ay = a.y;
+        int bx = b.x;
+        int by = b.y;
+
         // change index
-        gridItems[a.x, a.y] = b;
-        gridItems[b.x, b.y] = a;
+        int tempIndex = a.boxIndex;
+        a.boxIndex = b.boxIndex;
+        b.boxIndex = tempIndex;
 
         // change kordinates
-        int tempX = a.x;
-        int tempY = a.y;
-        a.x = b.x;
-        a.y = b.y;
-        b.x = tempX;
-        b.y = tempY;
+        a.x = bx;
+        a.y = by;
+        b.x = ax;
+        b.y = ay;
 
         // change pos
         a.transform.position = new Vector3(a.x * spacing, a.y * spacing, 0);
         b.transform.position = new Vector3(b.x * spacing, b.y * spacing, 0);
 
         // change boxIndex
-        int tempIndex = a.boxIndex;
-        a.boxIndex = b.boxIndex;
-        b.boxIndex = tempIndex;
+        gridItems[a.x, a.y] = a;
+        gridItems[b.x, b.y] = b;
+
+        Debug.Log($"Swapped items: a({a.x},{a.y}) boxIndex={a.boxIndex}, b({b.x},{b.y}) boxIndex={b.boxIndex}");
+
+        if (CheckThreeInARow())
+            Debug.Log("Three in a row found!");
+        else
+            Debug.Log("No three in a row.");
     }
+
+
+
+    public bool CheckThreeInARow()
+{
+    for (int y = 0; y < size; y++)
+    {
+        for (int x = 0; x < size - 2; x++)
+        {
+            int idx = gridItems[x, y].boxIndex;
+            if (idx == -1) continue;
+
+            if (gridItems[x + 1, y].boxIndex == idx &&
+                gridItems[x + 2, y].boxIndex == idx)
+            {
+                Debug.Log($"3 in a row at ({x},{y}), ({x+1},{y}), ({x+2},{y}) with index {idx}");
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
 }
